373ac9f5eff3e52ae343bd30098810383f682d7c
Ileana Padilla <Ile@Ileanas-Mac.local>
2014-01-21 19:23:28 -0600

    Initial Commit

---
+++ UAG8010/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UAG8010
+//
+//  Created by Ileana Padilla on 1/21/14.
+//  Copyright (c) 2014 Ileana Padilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ UAG8010/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UAG8010
+//
+//  Created by Ileana Padilla on 1/21/14.
+//  Copyright (c) 2014 Ileana Padilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
